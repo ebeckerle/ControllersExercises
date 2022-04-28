2b31ed4f0090a3f51a270ba21c00f9161919fd92
Elizabeth <elzbthyorke@gmail.com>
2022-04-28 10:12:11 -0500

    "10.5 Exercises - Controllers and Routing"

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -9,15 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("hello")
 public class HelloController {
 
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Fox!";
-//    }
 
     // url now looks like /hello/goodbye
     @GetMapping("goodbye")
-//    @ResponseBody
+
     public String goodbye(){
         return "Goodbye, Fox!, I love you!";
     }
@@ -25,21 +20,18 @@ public class HelloController {
     // create handler that handles request of the form /hello?name=LaunchCode
 //    @GetMapping("hello")
     @RequestMapping(method={RequestMethod.GET, RequestMethod.POST})
-//    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, "+ name +"!";
     }
 
     //Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-//    @ResponseBody
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
 //        return "redirect:/DESIREDPATH";
     }
 
     @GetMapping("form")
-//    @ResponseBody
     public String helloForm(){
         return "<html>" +
                 "<body>" +
@@ -50,4 +42,6 @@ public class HelloController {
                 "</body" +
                 "</html>";
     }
+
+
 }
